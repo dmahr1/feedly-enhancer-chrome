7596bc8559610423a13e0406a9947d276c026b8a
dmahr1 <danmahr23@gmail.com>
2015-12-09 13:41:42 -0500

    Incremented version to 3.0.0
    Hopefully this will trigger TamperMonkey update

--- feedly-enhancer-chrome.js
+++ feedly-enhancer-chrome.js
@@ -12,7 +12,7 @@
 // @grant       GM_setValue
 // @downloadURL https://raw.githubusercontent.com/dmahr1/feedly-enhancer-chrome/master/feedly-enhancer-chrome.js
 // @updateURL   https://raw.githubusercontent.com/dmahr1/feedly-enhancer-chrome/master/feedly-enhancer-chrome.js
-// @version    	2.3.2
+// @version    	3.0.0
 // @run-at     document-end
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @license    	Creative Commons; http://creativecommons.org/licenses/by-nc-sa/3.0/
